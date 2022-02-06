5c5357e40890e5f70e15752540e2bb96461668ce
Haoran Zhou <kens0serious@gmail.com>
2022-02-06 00:41:18 -0500

    context setup

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque dolorum dignissimos quas a provident, voluptatibus voluptate odit
+        esse eius labore eaque veritatis cupiditate autem inventore placeat
+        nihil beatae nesciunt cumque asperiores recusandae saepe tempora.
+        Cupiditate quas ab nostrum similique?
+      </p>
+    </section>
   )
 }
 
